32205452ed85d1c20c5b522e3494cf0e459c3ecc
Мария <mariaoskarr@gmail.com>
2018-11-25 16:13:40 +0300

    Initial commit

---
+++ app/src/androidTest/java/ru/tinkoff/uiautomator/application/YouTube.kt
@@ -0,0 +1,3 @@
+package ru.tinkoff.uiautomator.application
+
+class YouTube : AbstractApplication("com.google.android.youtube")
---
+++ app/src/androidTest/java/ru/tinkoff/uiautomator/test/YoutubeTest.kt
@@ -0,0 +1,5 @@
+package ru.tinkoff.uiautomator.test
+
+import ru.tinkoff.uiautomator.application.YouTube
+
+class YoutubeTest : AbstractApplicationTest<YouTube>(YouTube())
